0863ebbb45e11e764fa54ee018a5cf9a47b69cb4
Chayon <namelesscc159@gmail.com>
2025-09-08 20:46:02 +0600

    Add files via upload

---
+++ aus.js
@@ -0,0 +1,19 @@
+function start() {
+  let audio_effect = new Audio("music.MP3");
+  audio_effect.play()
+  let recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
+  recognition.lang = "en-AU";
+  let output = document.getElementById("textarea");
+
+  recognition.onstart = function () {
+    output.value = "I can hear you........";
+  };
+
+  recognition.onresult = function (event) {
+    let text = event.results[0][0].transcript;
+    output.value = text;
+  };
+
+  recognition.start();
+}
